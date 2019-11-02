d6f40059c491c3f1c148424a55251e8d487a86b7
Paola Dominguez <paoladomdiaz@gmail.com>
2019-11-01 23:08:38 -0600

    Add: paginas principales

---
+++ src/pages/Menu/Menu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../../components/Layout/Layout';
+
+const Menu = () => {
+    return (
+        <Layout>
+            <h2> Menu </h2>
+        </Layout>
+    );
+}
+
+export default Menu;
