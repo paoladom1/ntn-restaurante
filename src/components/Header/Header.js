b2d0045c50eb87df95c53999d0074460033c04fa
Paola Dominguez <paoladomdiaz@gmail.com>
2019-11-03 00:18:55 -0600

    add: estilo a la barra de navegacion

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,36 +1,17 @@
-import React from 'react';
-import {
-    Link
-} from 'react-router-dom';
-import {
-    Layout,
-    Menu
-} from 'antd';
+import React from "react";
+import { Layout, Icon } from "antd";
+import NavMenu from "../NavMenu/NavMenu";
+import styles from "./Header.module.css";
 
-const { Header: AntdHeader} = Layout;
+const { Header: AntdHeader } = Layout;
 
 const Header = () => {
-    return (
-        <AntdHeader>
-            <Menu mode='horizontal' defaultSelectedKeys={['home']}>
-                <Menu.Item key='home'>
-                    <Link to='/'>
-                        Home
-                    </Link>
-                </Menu.Item>
-                <Menu.Item key='menu'>
-                    <Link to='/menu'>
-                        Menu
-                    </Link>
-                </Menu.Item>
-                <Menu.Item key='about'>
-                    <Link to='/about'>
-                        About
-                    </Link>
-                </Menu.Item>
-            </Menu>
-        </AntdHeader>
-    )
-}
+  return (
+    <AntdHeader className={styles.header}>
+      <Icon type="appstore" /> {/*cambiar por logo de la aplicacion*/}
+      <NavMenu className={styles.navmenu} />
+    </AntdHeader>
+  );
+};
 
 export default Header;
