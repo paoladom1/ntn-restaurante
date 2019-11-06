10f9994e605f836c629f9e859b23407fce731a89
AlexisMejia <00090718@uca.edu.sv>
2019-11-06 15:12:08 -0600

    Footer: Fixing sass

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import { Icon } from 'antd';
+import { Icon, Layout } from 'antd';
 import styles from './Footer.module.scss';
 
+const { Footer: AntFooter } = Layout;
 
 const Footer = () => {
     return (
-        <footer>
+        <AntFooter>
             <nav>
                 <ul className={styles.social}>
                     <li>
                         <a href="https://www.facebook.com">
-                        <Icon type="facebook" theme="filled" />
+                            <Icon type="facebook" theme="filled" />
                         </a>
                     </li>
                     <li>
                         <a href="https://twitter.com">
-                        <Icon type="twitter-circle" theme="filled" />
+                            <Icon type="twitter-circle" theme="filled" />
                         </a>
                     </li>
                 </ul>
             </nav>
-        </footer>
+            <div className={styles.down}>
+                <strong>NTN Restaurant 2019</strong>    <Icon type="copyright-circle" theme="filled" />
+            </div>
+        </AntFooter>
     );
 }
 
