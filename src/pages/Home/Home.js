c008f924edd045e695e6a4a444c408097eaaa7a1
AlexisMejia <00090718@uca.edu.sv>
2019-11-06 15:14:01 -0600

    Home: adding Card

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Layout from '../../components/Layout/Layout';
 import BigHero from '../../components/BigHero/BigHero';
+import RestaurantInfoList from '../../components/Card/RestaurantInfoList';
 
 const Home = () => {
     return (
         <Layout>
             <BigHero>
             </BigHero>
+            <RestaurantInfoList>
+            </RestaurantInfoList>
+
         </Layout>
     );
 }
