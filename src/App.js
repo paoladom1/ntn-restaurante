4b4889f4205acb52a5b6229f698fca79c1f099a8
Paola Dominguez <paoladomdiaz@gmail.com>
2019-11-06 23:04:13 -0600

    add: customize antd theme

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
-import AppRouter from './AppRouter';
+import AppRouter from "./AppRouter";
+import { CloudinaryContext } from "cloudinary-react";
 
 export default function App() {
   return (
-    <AppRouter />
+    <CloudinaryContext cloudName="dw8gae3co">
+      <AppRouter />
+    </CloudinaryContext>
   );
 }
