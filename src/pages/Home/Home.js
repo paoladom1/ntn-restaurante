aafc9753338a69ee6474012e41f1c91040b81a36
AlexisMejia <00090718@uca.edu.sv>
2019-11-08 02:50:09 -0600

    Adding components

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Layout from '../../components/Layout/Layout';
 import BigHero from '../../components/BigHero/BigHero';
-import RestaurantInfoList from '../../components/Card/RestaurantInfoList';
-
+import SucursalesHeader from '../../components/Sucursales/SucursalesHeader';
+import FormularioRest from '../../components/Formulario/FormularioRest';
 const Home = () => {
     return (
         <Layout>
             <BigHero />
-            {/*<RestaurantInfoList />*/}
+            <SucursalesHeader />
+           {/*<FormularioRest/>*/}
         </Layout>
     );
 }
