ffd5b7466b7c113c5f6d4167f697053a7adaf68d
AlexisMejia <00090718@uca.edu.sv>
2019-11-08 02:48:11 -0600

    List: adding restaurants

---
+++ src/components/List/RestaurantInfoList.js
@@ -0,0 +1,45 @@
+import React from "react"
+import { List, Card } from 'antd';
+import CardInfo from '../Card/CardInfo';
+import styles from "./RestaurantInfoList.module.scss";
+
+const data = [
+    {
+        title: 'Santa Tecla',
+    },
+    {
+        title: 'San Salvador',
+    },
+    {
+        title: 'Santa Ana',
+    },
+];
+
+const ListInfo = () => {
+    return (
+        <div className={styles.inner}>
+            <h1>SUCURSALES</h1>
+            <List className={styles.ls}
+                grid={{
+                    gutter: 16,
+                    xs: 1,
+                    sm: 2,
+                    md: 4,
+                    lg: 4,
+                    xl: 6,
+                    xxl: 3,
+
+                }}
+                dataSource={data}
+                renderItem={item => (
+                    <List.Item>
+                        <Card className={styles.cardi} title={item.title}>Card content</Card>
+                    </List.Item>
+                )}
+            >
+            </List>
+        </div>
+    );
+}
+
+export default ListInfo;
