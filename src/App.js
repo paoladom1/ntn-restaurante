e9d4848fa6e0ff09c2e8abf8c725847dd83152cb
Paola Dominguez <paoladomdiaz@gmail.com>
2019-11-10 20:25:18 -0600

    add: fontawesome library configuration

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React from "react";
 import AppRouter from "./AppRouter";
 import { CloudinaryContext } from "cloudinary-react";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { faShoppingCart, faUtensils } from "@fortawesome/free-solid-svg-icons";
+
+library.add(fab, faShoppingCart, faUtensils);
+
 export default function App() {
-  return (
-    <CloudinaryContext cloudName="dw8gae3co">
-      <AppRouter />
-    </CloudinaryContext>
-  );
+    return (
+        <CloudinaryContext cloudName="dw8gae3co">
+            <AppRouter />
+        </CloudinaryContext>
+    );
 }
