2462451c005c4f913d4b5dbc0a23ad6dc76d041c
Paola Dominguez <paoladomdiaz@gmail.com>
2019-11-10 20:27:59 -0600

    update: header and footer styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,32 +1,33 @@
-import React from 'react';
-import { Icon, Layout } from 'antd';
-import styles from './Footer.module.scss';
+import React from "react";
+import { Icon, Layout } from "antd";
+import styles from "./Footer.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const { Footer: AntFooter } = Layout;
 
 const Footer = () => {
-    return (
-        <AntFooter>
-            <nav>
-                <ul className={styles.social}>
-                    <li>
-                        <a href="https://www.facebook.com">
-                            <Icon type="facebook" theme="filled" />
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://twitter.com">
-                            <Icon type="twitter-circle" theme="filled" />
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-            <div className={styles.down}>
-                <strong>NTN Restaurant 2019</strong>    <Icon type="copyright-circle" theme="filled" />
-            </div>
-        </AntFooter>
-    );
-}
-
+  return (
+    <AntFooter>
+      <nav>
+        <ul className={styles.social}>
+          <li>
+            <a href="https://www.facebook.com">
+              <FontAwesomeIcon className={styles.icon} icon={["fab", "facebook"]} />
+            </a>
+          </li>
+          <li>
+            <a href="https://twitter.com">
+            <FontAwesomeIcon className={styles.icon} icon={["fab", "twitter"]} />
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div className={styles.down}>
+        <p>NTN Restaurant 2019</p>
+        <Icon className={styles.icon} type="copyright" theme="outlined" />
+      </div>
+    </AntFooter>
+  );
+};
 
 export default Footer;
