bb2505c9aded10be07b2c43f651386f1f912f7cb
AlexisMejia <00090718@uca.edu.sv>
2019-11-10 23:32:06 -0600

    RestaurantInfoList: modified style

--- src/components/List/RestaurantInfoList.js
+++ src/components/List/RestaurantInfoList.js
@@ -24,9 +24,9 @@ const ListInfo = () => {
                     gutter: 16,
                     xs: 1,
                     sm: 2,
-                    md: 4,
-                    lg: 4,
-                    xl: 6,
+                    md: 3,
+                    lg: 3,
+                    xl: 3,
                     xxl: 3,
 
                 }}
