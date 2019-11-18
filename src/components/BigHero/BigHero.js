aadd81f3647170ab22416c59ba22d2afe6adff6f
AlexisMejia <00090718@uca.edu.sv>
2019-11-18 13:00:10 -0600

    BigHero: information

--- src/components/BigHero/BigHero.js
+++ src/components/BigHero/BigHero.js
@@ -8,10 +8,13 @@ const BigHero = () => {
           <div className={styles.inner}>
               <header className={styles.head}>
                   <h1>¡Bienvenido!</h1>
+                  <hr/>
               </header>
               <div className={styles.content}>
                   <p>Cada vez que abrimos nuestras puertas nos esforzamos en atenderte a la altura que esperas,
-                     y así prefieras más nuestros restaurantes.</p>
+                     y así prefieras más nuestros restaurantes. <br/> 
+                     Gracias a ti hoy en dia somos lo que somos y por ello hemos decidido innovarnos, manteniendo nuestra calidad,
+                     servicio y excelencia <br/> pero abriendole los brazos a la teconologia.</p>
               </div>
           </div>
       </section>
