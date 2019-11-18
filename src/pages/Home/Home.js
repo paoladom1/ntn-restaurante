ea8cbe865ace891e19e0c1c1182488ed0fd23754
AlexisMejia <00090718@uca.edu.sv>
2019-11-18 12:05:16 -0600

    Home: adding component

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,13 +2,13 @@ import React from 'react';
 import Layout from '../../components/Layout/Layout';
 import BigHero from '../../components/BigHero/BigHero';
 import SucursalesHeader from '../../components/Sucursales/SucursalesHeader';
-import FormularioRest from '../../components/Formulario/FormularioRest';
+import FormularioNew from '../../components/Formulario/FormularioNew';
 const Home = () => {
     return (
         <Layout>
             <BigHero />
             <SucursalesHeader />
-           <FormularioRest/>
+           <FormularioNew/>
         </Layout>
     );
 }
