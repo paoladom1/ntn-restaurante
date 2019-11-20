e6beea88117af5c1d2e2f3b79c720d6d1135c346
AlexisMejia <00090718@uca.edu.sv>
2019-11-20 16:02:14 -0600

    AboutBigHero: adding information

--- src/components/About/AboutBigHero/AboutBigHero.js
+++ src/components/About/AboutBigHero/AboutBigHero.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Image, Transformation } from "cloudinary-react";
 import styles from "./AboutBigHero.module.scss";
 
 
@@ -7,14 +8,27 @@ const AboutBigHero = () => {
         <div className={styles.wrapper}>
             <section className={styles.big}>
                 <div className={styles.inner}>
-                    <header className={styles.head}>
+                    <div className={styles.information}>
                         <h1>Conócenos</h1>
-                    </header>
-                    <div className={styles.content}>
-                        <p>Por sus múltiples y deliciosos platillos y, en especial por sus famosos cortes de carne a la parrilla y deliciosos postres, NTN Restaurante es el restaurante obligado e indiscutible cuando se busca calidad y perfección; no en vano puede presumir con tanto orgullo de haber brindado un excelente servicio y exquisita comida desde 1987.</p>
+                        <hr color="black"/>
+                        <div className={styles.content}>
+                            <p>Por sus múltiples y deliciosos platillos y,en especial por sus famosos cortes de carne a la parrilla y deliciosos postres,
+                                NTN Restaurante es el restaurante obligado e indiscutible cuando se busca calidad y perfección;</p>
+                        </div>
+                    </div>
+                    <div className={styles.img}>
+                        <div className={styles.im1}>
+                            <Image publicId={"/proyecto/img3"} className={styles.imgH} />
+                        </div>
+                        <div className={styles.im2}>
+                            <Image publicId={"/proyecto/img5"} className={styles.imgH} />
+                        </div>
+                        <div className={styles.im3}>
+                            <Image publicId={"/proyecto/img4"} className={styles.imgH} />
+                        </div>
                     </div>
                 </div>
-                <div className={styles.sesgoabajo}/>
+                <div className={styles.sesgoabajo} />
             </section>
         </div>
     );
