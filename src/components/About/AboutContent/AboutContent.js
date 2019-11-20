9abef2acb1186f6dfc3bf6f28f287343dcae7eb7
AlexisMejia <00090718@uca.edu.sv>
2019-11-20 13:42:19 -0600

    AboutContent: adding information

--- src/components/About/AboutContent/AboutContent.js
+++ src/components/About/AboutContent/AboutContent.js
@@ -9,7 +9,7 @@ const AboutContent = () => {
                 <div className={styles.contenedor}>
                     <h2 className={styles.sobrenosotros}>Sobre nosotros</h2>
                     <hr />
-                    <p className={styles.parrafo}>Desde su apertura, Restaurantes NTN fue reconocido por la calidad de sus platos y precios altamente competitivos.<br /><br />
+                    <p>Desde su apertura, Restaurantes NTN fue reconocido por la calidad de sus platos y precios altamente competitivos.<br/>
                         Los resultados de tradición y calidad no sólo se encuentran en la comida, sino en el cálido ambiente que inunda el lugar. Su tamaño, detalles y ambientación son la combinación ideal para que nuestros clientes se encuentren cómodos y en confianza</p>
                 </div>  
             </section>
@@ -24,7 +24,7 @@ const AboutContent = () => {
                     </div>
                     <div className={styles.vision}>
                         <h2 className={styles.sobrenosotros}>Vision</h2>
-                        <hr />
+                        <hr/>
                         <p>Ser la empresa líder a nivel regional en el negocio de Restaurantes tipo Steak House.</p>
                     </div>
                 </div>
@@ -32,10 +32,10 @@ const AboutContent = () => {
             <section className={styles.acercaUs}>
                 <div className={styles.sesgo} />
                 <div className={styles.contenedor}>
-                    <h2 className={styles.sobrenosotros}>Welcomeg</h2>
+                    <h2 className={styles.sobrenosotros}>Welcome</h2>
                     <hr />
                     <p className={styles.parrafo}>Te damos la bienvenida a tu casa “NTN Restaurante”,conoce más acerca de nuestros platillos preparados con los mayores estándares de calidad.
-                    Nuestro compromiso es suplir todas tus expectativas y hacer que en tu visita vivas la mejor experiencia.<br/><br/>
+                    Nuestro compromiso es suplir todas tus expectativas y hacer que en tu visita vivas la mejor experiencia.<br/>
                     Por sus múltiples y deliciosos platillos y, en especial por sus famosos cortes de carne a la parrilla y deliciosos postres, NTN Restaurante es el restaurante obligado e indiscutible cuando se busca calidad y perfección; no en vano puede presumir con tanto orgullo de haber brindado un excelente servicio y exquisita comida desde 1987.</p>
                 </div>
             </section>
