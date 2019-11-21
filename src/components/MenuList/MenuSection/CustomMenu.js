8be74971971eedd49a3fd78a00d5d1bfa6933efd
Paola Dominguez <paoladomdiaz@gmail.com>
2019-11-21 14:55:33 -0600

    fix: issues

--- src/components/MenuList/MenuSection/CustomMenu.js
+++ src/components/MenuList/MenuSection/CustomMenu.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { Row, Col, Icon } from "antd";
+import { Row, Col, Icon, Button } from "antd";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import notification from "./../../Notification/Notification";
 import styles from "./CustomMenu.module.scss";
 import { AppContext } from "../../../AppProvider";
+import { withRouter } from "react-router-dom";
 
 class CustomMenu extends React.Component {
     constructor(props) {
@@ -38,117 +39,109 @@ class CustomMenu extends React.Component {
             <AppContext.Consumer>
                 {({ cart, updateCart }) => (
                     <div className={styles.wrapper}>
-                            <Row className={styles.cardContent}>
+                        <Row className={styles.cardContent}>
                             <div>
-                            <Button
-                                className={styles.goBack}
-                                onClick={() => {
-                                console.log(location);
-                                history.push("/menu");
-                            }} >
-                                 <FontAwesomeIcon
-                                     icon="arrow-left" className={styles.Icon}/>
-                            </Button>
-                        </div> 
-                                <Col
-                                    className={styles.cardMenu}
-                                    xs={24}
-                                    md={24}
-                                    lg={24}
+                                <Button
+                                    className={styles.goBack}
+                                    onClick={() => {
+                                        console.log(location);
+                                        history.push("/menu");
+                                    }}
                                 >
-                                    <div className={styles.cardTitle}>
-                                        <FontAwesomeIcon
-                                            className={styles.icon}
-                                            icon="utensils"
-                                        />
-                                        <h2 className={styles.title}>
-                                            {this.props.name}
-                                        </h2>
-                                    </div>
-                                    <div className={styles.listContent}>
-                                        {this.state.foodList.map(
-                                            (food, index) => {
-                                                return (
-                                                    <div key={food.name}>
-                                                        <Row gutter={16}>
-                                                            <Col
-                                                                xs={18}
-                                                                md={21}
-                                                            >
-                                                                <h3
-                                                                    className={
-                                                                        styles.foodName
-                                                                    }
-                                                                >
-                                                                    {food.name}
-                                                                </h3>
-                                                                <p
-                                                                    className={
-                                                                        styles.foodDescription
-                                                                    }
-                                                                >
-                                                                    {
-                                                                        food.description
-                                                                    }
-                                                                </p>
-                                                            </Col>
-                                                            <Col xs={4} md={2}>
-                                                                <span
-                                                                    className={
-                                                                        styles.foodPrice
-                                                                    }
-                                                                >
-                                                                    $
-                                                                    {Number(
-                                                                        food.price
-                                                                    ).toFixed(
-                                                                        2
-                                                                    )}
-                                                                </span>
-                                                            </Col>
-                                                            <Col
-                                                                xs={2}
-                                                                md={1}
-                                                                className={
-                                                                    styles.addToCart
-                                                                }
-                                                            >
-                                                                <Icon
-                                                                    className={
-                                                                        styles.icon
-                                                                    }
-                                                                    type="plus-circle"
-                                                                    theme="filled"
-                                                                    onClick={() => {
-                                                                        updateCart(
-                                                                            cart.concat(
-                                                                                [
-                                                                                    food
-                                                                                ]
-                                                                            )
-                                                                        );
+                                    <FontAwesomeIcon
+                                        icon="arrow-left"
+                                        className={styles.Icon}
+                                    />
+                                </Button>
+                            </div>
+                            <Col
+                                className={styles.cardMenu}
+                                xs={24}
+                                md={24}
+                                lg={24}
+                            >
+                                <div className={styles.cardTitle}>
+                                    <FontAwesomeIcon
+                                        className={styles.icon}
+                                        icon="utensils"
+                                    />
+                                    <h2 className={styles.title}>
+                                        {this.props.name}
+                                    </h2>
+                                </div>
+                                <div className={styles.listContent}>
+                                    {this.state.foodList.map((food, index) => {
+                                        return (
+                                            <div key={food.name}>
+                                                <Row gutter={16}>
+                                                    <Col xs={18} md={21}>
+                                                        <h3
+                                                            className={
+                                                                styles.foodName
+                                                            }
+                                                        >
+                                                            {food.name}
+                                                        </h3>
+                                                        <p
+                                                            className={
+                                                                styles.foodDescription
+                                                            }
+                                                        >
+                                                            {food.description}
+                                                        </p>
+                                                    </Col>
+                                                    <Col xs={4} md={2}>
+                                                        <span
+                                                            className={
+                                                                styles.foodPrice
+                                                            }
+                                                        >
+                                                            $
+                                                            {Number(
+                                                                food.price
+                                                            ).toFixed(2)}
+                                                        </span>
+                                                    </Col>
+                                                    <Col
+                                                        xs={2}
+                                                        md={1}
+                                                        className={
+                                                            styles.addToCart
+                                                        }
+                                                    >
+                                                        <Icon
+                                                            className={
+                                                                styles.icon
+                                                            }
+                                                            type="plus-circle"
+                                                            theme="filled"
+                                                            onClick={() => {
+                                                                updateCart(
+                                                                    cart.concat(
+                                                                        [food]
+                                                                    )
+                                                                );
 
-                                                                        notification(
-                                                                            "Item agregado",
-                                                                            food.description,
-                                                                            "success",
-                                                                            2
-                                                                        );
-                                                                    }}
-                                                                />
-                                                            </Col>
-                                                        </Row>
-                                                    </div>
-                                                );
-                                            }
-                                        )}
-                                    </div>
-                                </Col>
-                            </Row>
+                                                                notification(
+                                                                    "Item agregado",
+                                                                    food.description,
+                                                                    "success",
+                                                                    2
+                                                                );
+                                                            }}
+                                                        />
+                                                    </Col>
+                                                </Row>
+                                            </div>
+                                        );
+                                    })}
+                                </div>
+                            </Col>
+                        </Row>
                     </div>
                 )}
             </AppContext.Consumer>
-        )
+        );
     }
 }
 
